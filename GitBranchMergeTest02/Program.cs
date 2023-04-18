e0bb0e4a99ec613756091d696aebd46781267b8e
jhon65496 <it1233@yandex.ru>
2023-04-18 11:55:51 +0300

    br-2 | Modific-3

--- GitBranchMergeTest02/Program.cs
+++ GitBranchMergeTest02/Program.cs
@@ -8,7 +8,7 @@ namespace GitBranchMergeTest02
 {
     internal class Program
     {
-        // br-2 | Modific-2
+        // br-2 | Modific-3
         static void Main(string[] args)
         {
         }
